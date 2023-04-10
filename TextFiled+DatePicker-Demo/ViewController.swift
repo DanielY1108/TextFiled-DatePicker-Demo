25fecb0dba455316725f49020e66571b5e966e92
DanielY1108 <scarlet040@gmail.com>
2023-04-10 18:43:16 +0900

    텍스트필드에 데이터피커를 적용시켜보았습니다. 툴바도 추가 함

--- TextFiled+DatePicker-Demo/ViewController.swift
+++ TextFiled+DatePicker-Demo/ViewController.swift
@@ -8,12 +8,120 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // UIDatePicker 객체 생성을 해줍니다.
+    let datePicker = UIDatePicker()
+
+    let textField: UITextField = {
+        let field = UITextField()
+        field.backgroundColor = .secondarySystemBackground
+        field.layer.cornerRadius = 10
+        return field
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+
+        setupTextField()
+        setupToolBar()
+        setupDatePicker()
+    }
+    
+    private func setupDatePicker() {
+        // datePickerModed에는 time, date, dateAndTime, countDownTimer가 존재합니다.
+        datePicker.datePickerMode = .date
+        // datePicker 스타일을 설정합니다. wheels, inline, compact, automatic이 존재합니다.
+        datePicker.preferredDatePickerStyle = .wheels
+        // 값이 변할 때마다 동작을 설정해 줌
+        datePicker.addTarget(self, action: #selector(dateChange), for: .valueChanged)
+        // textField의 inputView가 nil이라면 기본 할당은 키보드입니다.
+        textField.inputView = datePicker
+        // textField에 오늘 날짜로 표시되게 설정
+        textField.text = dateFormat(date: Date())
+    }
+    
+    // 값이 변할 때 마다 동작
+    @objc func dateChange(_ sender: UIDatePicker) {
+        // 값이 변하면 UIDatePicker에서 날자를 받아와 형식을 변형해서 textField에 넣어줍니다.
+        textField.text = dateFormat(date: sender.date)
+    }
+    
+    // 텍스트 필드에 들어갈 텍스트를 DateFormatter 변환
+    private func dateFormat(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy / MM / dd"
+        
+        return formatter.string(from: date)
     }
 
+    // 툴바 세팅
+    private func setupToolBar() {
+        
+        let toolBar = UIToolbar()
 
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonHandeler))
+
+        toolBar.items = [flexibleSpace, doneButton]
+        // 적절한 사이즈로 toolBar의 크기를 만들어 줍니다.
+        toolBar.sizeToFit()
+
+        // textField의 경우 클릭 시 키보드 위에 AccessoryView가 표시된다고 합니다.
+        // 현재 inputView를 datePicker로 만들어 줬으니 datePicker위에 표시되겠죠?
+        textField.inputAccessoryView = toolBar
+    }
+    
+    @objc func doneButtonHandeler(_ sender: UIBarButtonItem) {
+        textField.text = dateFormat(date: datePicker.date)
+        // 키보드 내리기
+        textField.resignFirstResponder()
+    }
+    
+    private func setupTextField() {
+        
+        view.addSubview(textField)
+        textField.frame = CGRect(x: 50,
+                                 y: 300,
+                                 width: view.frame.size.width-100,
+                                 height: 50)
+    }
+    
 }
 
+
+
+
+
+
+#if DEBUG
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+        let viewController: UIViewController
+        
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+    }
+    
+    func toPreview() -> some View {
+        Preview(viewController: self)
+    }
+}
+#endif
+
+
+// MARK: - PreView 읽기
+import SwiftUI
+
+#if DEBUG
+struct PreView: PreviewProvider {
+    static var previews: some View {
+        // 사용할 뷰 컨트롤러를 넣어주세요
+        ViewController()
+            .toPreview()
+    }
+}
+#endif
